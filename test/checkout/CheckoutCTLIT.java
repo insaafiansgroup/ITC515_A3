62bdddb67f7ba9a392a906401b454740b9c25b98
insaafiansgroup <41322754+insaafiansgroup@users.noreply.github.com>
2018-09-21 17:14:06 +1000

    Create a test class of Checkout.java for Integration testing
    Create a test class of Checkout.java for Integration testing

--- test/checkout/CheckoutCTLIT.java
+++ test/checkout/CheckoutCTLIT.java
@@ -1,1 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel.checkout;
+
+import hotel.credit.CreditCardType;
+import hotel.entities.Hotel;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Malik
+ */
+public class CheckoutCTLIT {
+    
+    public CheckoutCTLIT() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of creditDetailsEntered method, of class CheckoutCTL.
+     */
+    @Test
+    public void testCreditDetailsEntered() { // Integration Testing
+       
+    }
+  
+    
+}
 
