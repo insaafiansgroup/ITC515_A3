869b7962d715c2e1741665fe9312e25fd4c844a3
insaafiansgroup <41322754+insaafiansgroup@users.noreply.github.com>
2018-09-21 17:23:36 +1000

    Create test method testCheckIn()
    Create test method testCheckIn()

--- test/enitities/BookingTest.java
+++ test/enitities/BookingTest.java
@@ -1,1 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel.entities;
+
+import hotel.credit.CreditCard;
+import hotel.credit.CreditCardType;
+import java.util.Date;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Malik
+ */
+public class BookingTest {
+    
+    
+    public BookingTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+ 
+    /**
+     * Test of checkIn method, of class Booking.
+     */
+    @Test
+    public void testCheckIn() {
+       
+    }
+
+    
+    
+}
 
